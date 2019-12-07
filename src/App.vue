7cb8e1bad6b7e07ddad14c804723f948642519a1
Farhan <frhnmdni@gmail.com>
2019-12-07 15:46:16 +0530

    add filter todos functionality

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <AddTodo />
+    <FilterTodos />
     <Todos />
   </div>
 </template>
@@ -8,11 +9,13 @@
 <script>
 import Todos from "./components/Todos";
 import AddTodo from "./components/AddTodo";
+import FilterTodos from "./components/FilterTodos";
 export default {
   name: "app",
   components: {
     Todos,
-    AddTodo
+    AddTodo,
+    FilterTodos
   }
 };
 </script>
--- src/store/modules/ToDos.js
+++ src/store/modules/ToDos.js
@@ -10,7 +10,7 @@ const getters = {
 
 const actions = {
   async fetchTodos({ commit }) {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10');
+    const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
     commit('setTodos', response.data);
   },
   async addTodo({ commit }, title) {
@@ -20,6 +20,11 @@ const actions = {
   async deleteTodo({ commit }, id) {
     await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
     commit('removeTodo', id);
+  },
+  async filterTodos({ commit }, e) {
+    const limit = parseInt(e.target.options[e.target.options.selectedIndex].innerText);
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/todos?_limit=${limit}`);
+    commit('setTodos', response.data);
   }
 };
 
